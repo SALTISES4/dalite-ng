5a20095022703afdbe86b48609ce6cae47b9f5f5
sambhatnagar <sameer.bhatnagar@gmail.com>
2021-07-16 18:02:54 -0400

    add institution field to group

--- peerinst/models/group.py
+++ peerinst/models/group.py
@@ -7,6 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from quality.models import Quality
 
+from .institution import Institution
 from .question import Discipline
 
 
@@ -58,6 +59,9 @@ class StudentGroup(models.Model):
 
     discipline = models.ForeignKey(
         Discipline, blank=True, null=True, on_delete=models.SET_NULL
+    )
+    institution = models.ForeignKey(
+        Institution, blank=True, null=True, on_delete=models.SET_NULL
     )
 
     def __str__(self):
