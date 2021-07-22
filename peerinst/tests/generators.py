1cb831cd3de3b7f584993de6447276cc72fb93c8
sambhatnagar <sameer.bhatnagar@gmail.com>
2021-07-21 22:50:37 -0400

    update test/generators/add_to_group with get_or_create

--- peerinst/tests/generators.py
+++ peerinst/tests/generators.py
@@ -358,7 +358,9 @@ def add_to_group(students, groups):
     for student in students:
         student.groups.add(*groups)
         for group in groups:
-            StudentGroupMembership.objects.create(student=student, group=group)
+            StudentGroupMembership.objects.get_or_create(
+                student=student, group=group
+            )
 
 
 def _extra_chars_gen():
