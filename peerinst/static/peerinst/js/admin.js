9548d1c937bec18a5a4dd12b921fe393f780baee
sambhatnagar <sameer.bhatnagar@gmail.com>
2021-07-22 13:36:32 -0400

    remove export of deleted file admin/activity.js

--- peerinst/static/peerinst/js/admin.js
+++ peerinst/static/peerinst/js/admin.js
@@ -2,4 +2,3 @@
 
 export { init as initNewUserApproval } from "./_admin/new_user_approval.js";
 export { init as initFlaggedRationales } from "./_admin/flagged_rationales.js";
-export { init as initActivity } from "./_admin/activity.js";
