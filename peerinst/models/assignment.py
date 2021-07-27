44b69eade5d473c6cb80a3bf9612b75337ce9317
jsumnerPhD <jonathon.r.e.sumner@gmail.com>
2021-07-26 21:52:12 -0400

    Rewrite includes_flagged_question using queries

--- peerinst/models/assignment.py
+++ peerinst/models/assignment.py
@@ -22,7 +22,7 @@ from ..util import (
 )
 from ..utils import format_time
 from .group import StudentGroup
-from .question import Question, QuestionFlag
+from .question import Question
 
 logger = logging.getLogger("peerinst-models")
 
@@ -148,24 +148,44 @@ class Assignment(models.Model):
 
     @property
     def includes_flagged_question(self):
-        return any(
-            [
-                0
-                in q.get_frequency(all_rationales=True)[
-                    "first_choice"
-                ].values()
-                for q in self.questions.all()
-            ]
-            + [
-                True
-                if q.pk
-                in QuestionFlag.objects.all().values_list(
-                    "question", flat=True
-                )
-                else False
-                for q in self.questions.all()
-            ]
+        """
+        Assignment is invalid if:
+        - A PI question does not have at least two answer choices
+        - A PI question does not have at least two sample answers (excluding
+          expert answers) for each answer choice
+        - Any question is flagged
+        """
+        pi_queryset = self.questions.filter(type="PI")
+
+        missing_answer_choices = (
+            pi_queryset.annotate(
+                answer_choice_count=models.Count("answerchoice")
+            )
+            .filter(answer_choice_count__lte=1)
+            .exists()
         )
+
+        missing_sample_answers = (
+            pi_queryset.values(
+                "pk", "answer__first_answer_choice", "answer__expert"
+            )
+            .exclude(answer__expert=True)
+            .annotate(
+                answer_count_for_choice=models.Count(
+                    "answer__first_answer_choice"
+                )
+            )
+            .filter(answer_count_for_choice__lte=1)
+            .exists()
+        )
+
+        flagged = (
+            self.questions.annotate(flagged=models.Count("questionflag"))
+            .filter(flagged__gt=0)
+            .exists()
+        )
+
+        return any([missing_answer_choices, missing_sample_answers, flagged])
 
 
 class AssignmentQuestions(models.Model):
