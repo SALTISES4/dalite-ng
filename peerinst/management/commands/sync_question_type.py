d68bcf3edd46d42e06b5d87c5bdfc493bcbbe3a1
jsumnerPhD <jonathon.r.e.sumner@gmail.com>
2021-09-08 23:41:43 -0400

    Add management command to fix misalignment between type and second_answer_needed

---
+++ peerinst/management/commands/sync_question_type.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+
+from peerinst.models import Question
+
+
+class Command(BaseCommand):
+    help = (
+        "Ensure alignment between question type and second_answer_needed "
+        "fields"
+    )
+
+    def handle(self, *args, **options):
+        count = 0
+        for q in Question.objects.filter(type="PI"):
+            if q.second_answer_needed is False:
+                q.second_answer_needed = True
+                q.save()
+                count += 1
+
+        print(f"{count} PI questions fixed.")
+
+        count = 0
+        for q in Question.objects.filter(type="RO"):
+            q.second_answer_needed = False
+            q.save()
+            count += 1
+
+        print(f"{count} RO questions fixed.")
