3a87070a49587ae2030b2c134e67b53bddbdb2a8
jsumnerPhD <jonathon.r.e.sumner@gmail.com>
2021-11-13 21:30:32 -0500

    New login workflow; build needs to be updated; some svg problems in Chrome

--- blink/urls.py
+++ blink/urls.py
@@ -3,7 +3,6 @@ from django.views.decorators.cache import cache_page
 
 from blink import views
 
-
 app_name = "blink"
 
 urlpatterns = [
@@ -17,23 +16,51 @@ urlpatterns = [
         views.BlinkQuestionDetailView.as_view(),
         name="blink-summary",
     ),
-    path("<int:pk>/count/", views.blink_count, name="blink-count",),
-    path("<int:pk>/close/", views.blink_close, name="blink-close",),
+    path(
+        "<int:pk>/count/",
+        views.blink_count,
+        name="blink-count",
+    ),
+    path(
+        "<int:pk>/close/",
+        views.blink_close,
+        name="blink-close",
+    ),
     path(
         "<int:pk>/latest_results/",
         views.blink_latest_results,
         name="blink-results",
     ),
-    path("<int:pk>/reset/", views.blink_reset, name="blink-reset",),
-    path("<int:pk>/status/", views.blink_status, name="blink-status",),
-    path("<username>/", views.blink_get_current, name="blink-get-current",),
+    path(
+        "<int:pk>/reset/",
+        views.blink_reset,
+        name="blink-reset",
+    ),
+    path(
+        "<int:pk>/status/",
+        views.blink_status,
+        name="blink-status",
+    ),
+    path(
+        "<username>/",
+        views.blink_get_current,
+        name="blink-get-current",
+    ),
     path(
         "<username>/url/",
         cache_page(1)(views.blink_get_current_url),
         name="blink-get-current-url",
     ),
-    path("<int:pk>/get_next/", views.blink_get_next, name="blink-get-next",),
-    path("waiting/<username>/", views.blink_waiting, name="blink-waiting",),
+    path(
+        "<int:pk>/get_next/",
+        views.blink_get_next,
+        name="blink-get-next",
+    ),
+    path(
+        "waiting/<username>/",
+        views.blink_waiting,
+        name="blink-waiting",
+    ),
     path(
         "waiting/<username>/<int:assignment>/",
         views.blink_waiting,
