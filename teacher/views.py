0a63bd882b7cd9481d7af2473f6784c0ef4a149a
sambhatnagar <sameer.bhatnagar@gmail.com>
2023-02-05 23:31:16 -0500

    avatar url for saltisemembers

--- teacher/views.py
+++ teacher/views.py
@@ -1,9 +1,26 @@
+import os
+
+from django.conf import settings
 from django.views.generic import DetailView
 
-from peerinst.models import Teacher
+from peerinst.models import SaltiseMember, Teacher
 from peerinst.views.views import TeacherBase
 
 
 class TeacherDashboardView(TeacherBase, DetailView):
     model = Teacher
     template_name = "teacher/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        backup_avatar = os.path.join(
+            settings.STATIC_URL, "components/img/logo.gif"
+        )
+        try:
+            if self.request.user.saltisemember.picture:
+                context["avatar"] = self.request.user.saltisemember.picture.url
+            else:
+                context["avatar"] = backup_avatar
+        except SaltiseMember.DoesNotExist:
+            context["avatar"] = backup_avatar
+        return context
