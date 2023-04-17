2d3568cdd2c8437ca114188b950172f89f889636
jsumnerPhD <jonathon.r.e.sumner@gmail.com>
2023-04-16 21:45:25 -0400

    Add static lists for disciplines, difficulties, and impacts to search context; support OR operations for difficulty, impact, and discipline queries (leave category as AND)

--- teacher/views.py
+++ teacher/views.py
@@ -1,6 +1,7 @@
+import bleach
 from django.views.generic import DetailView, TemplateView
 
-from peerinst.models import Teacher
+from peerinst.models import Discipline, Question, Teacher
 from peerinst.views.views import TeacherBase
 
 
@@ -13,6 +14,18 @@ class SearchView(TeacherBase, DetailView):
     http_method_names = ["get"]
     model = Teacher
     template_name = "teacher/search.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context.update(
+            difficulties=[[d[0], d[1]] for d in Question.DIFFICULTY_LABELS],
+            disciplines=[
+                bleach.clean(d, tags=[], strip=True).strip()
+                for d in Discipline.objects.values_list("title", flat=True)
+            ],
+            impacts=[[d[0], d[1]] for d in Question.PEER_IMPACT_LABELS],
+        )
+        return context
 
 
 class LibraryView(TeacherBase, TemplateView):
