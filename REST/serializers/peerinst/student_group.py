e9d5d1bc264a964b4cbf46111525ec8f42e88db3
sambhatnagar <sameer.bhatnagar@gmail.com>
2023-08-11 16:00:57 -0400

    connect teacher and group on group create

--- REST/serializers/peerinst/student_group.py
+++ REST/serializers/peerinst/student_group.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from peerinst.models import StudentGroup
+from peerinst.models import StudentGroup, Teacher
 
 from .dynamic_serializer import DynamicFieldsModelSerializer
 
@@ -10,6 +10,16 @@ class StudentGroupSerializer(DynamicFieldsModelSerializer):
 
     def get_url(self, obj):
         return obj.get_absolute_url()
+
+    def create(self, validated_data):
+        """Attach group to teacher"""
+        group = super().create(validated_data)
+        teacher = Teacher.objects.get(user=self.context["request"].user)
+        teacher.current_groups.add(group)
+        teacher.save()
+        group.teacher.add(teacher)
+        group.save()
+        return group
 
     class Meta:
         model = StudentGroup
