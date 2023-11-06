6740e3292e6c22a95489fdbe7a3f394544925d79
jsumnerPhD <jonathon.r.e.sumner@gmail.com>
2023-11-06 08:13:47 -0500

    Appbar for question preview

--- peerinst/static/peerinst/js/material-web.js
+++ peerinst/static/peerinst/js/material-web.js
@@ -1,4 +1,5 @@
 import "@material/web/icon/icon.js";
+import "@material/web/iconbutton/icon-button.js";
 import "@material/web/chips/assist-chip.js";
 import "@material/web/button/filled-button.js";
 import "@material/web/divider/divider.js";
