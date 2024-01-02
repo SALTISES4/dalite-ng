9bf20d454dfa0a7e28b3b6eeaa2941cb77714d42
jsumnerPhD <jonathon.r.e.sumner@gmail.com>
2024-01-02 16:33:25 -0500

    More permissive url pattern for assignment identifier

--- REST/views/content_creation/peerinst/assignment.py
+++ REST/views/content_creation/peerinst/assignment.py
@@ -19,7 +19,7 @@ class TeacherAssignmentCRUDViewSet(TeacherCRUDViewSet):
     """
 
     lookup_field = "identifier"
-    lookup_value_converter = "string"
+    lookup_value_regex = "[-a-zA-Z0-9_\.\s]+"
     serializer_class = AssignmentSerializer
 
     def get_queryset(self):
